f8128ffe976f649d48b6d3057757da02aa577b28
rosejam <dongju.alex@gmail.com>
2021-01-24 00:27:40 +0900

    assembly 마무리

--- Assembly/10.StackTest.asm
+++ Assembly/10.StackTest.asm
@@ -14,19 +14,42 @@ CMAIN:
     ; -- sp (Stack Pointer) : 현재 스택 top 위치 (일종의 cursor)
     ; -- bp (Base Pointer) : 스택 상대주소 계산용
     
-    push 1 ; 8byte의 1을 스택프레임에 넣는다.
+    push rax
+    push rbx
+    push 1 ; 8byte의 1를 스택프레임에 넣는다.
     push 2
-    push 3
     
-    pop rax ; pop한것을 -> rax에 넣어준다.
+    call MAX
+    PRINT_DEC 8, rax
+    NEWLINE
+    
+    ;pop rax ; pop한 2를 -> rax에 넣어준다.
+    ;pop rbx
+    ; push 두번 해놓은 거를 pop 두번 대신 sp에 16을 더해 해결할 수 있다!!!
+    add rsp, 16
     pop rbx
-    pop rcx
-    
+    pop rax
+            
     xor rax, rax
     ret ; main함수를 종료
     
+MAX: ; 스택 프레임으로 받은 두 인자 중 더 큰 값을 rax에 넣는다.
+    push rbp ; 패턴
+    mov rbp, rsp ; 패턴
+    
+    mov rax, [rbp+16] ; 2
+    mov rbx, [rbp+24] ; 1
+    
+    cmp rax, rbx
+    jg L1
+    mov rax, rbx
+L1:
 
-; [따라서!] 스택(stack)이라는 메모리 영역을 사용
+    pop rbp ; 패턴
+    ret
+    
+
+; [!] 스택(stack)이라는 메모리 영역을 사용
 ; 함수가 사용하는 일종의 메모장 (변수/인자들 저장)
 ; - 매개 변수 전달
 ; - 돌아갈 주소 관리
