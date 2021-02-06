e4cb8c02fabecf977f7e2efd2defd77e8fa26f03
rosejam <dongju.alex@gmail.com>
2021-02-06 16:33:09 +0900

    포인터 연산!

--- 16.구조체.cpp
+++ 16.구조체.cpp
@@ -12,19 +12,24 @@ using namespace std;
 // int monsterDefence;
 
 // 아래와 같이 플레이어, 몬스터 정보들을 한번에 묶어서 관리!
+// (어셈블리어에선 따로 변수를 만드는 것과 크게 다르지 않다!)
 // 컴퓨터가 연산을 위해 패딩을 넣기 때문에 구조체의 크기는 내용의 합보다 같거나 클 수 있다!
 struct ObjectInfo
 {
-    int type;
-    int hp;
-    int attack;
-    int defence;
-};
+    int type; // +0 (구조체 시작주소를 기준으로, 요소의 시작주소의 위치 (=오프셋)!!!!)
+    int hp; // +4
+    int attack; // +8
+    int defence; // +12
+}; // 총 16바이트 크기
+
 ObjectInfo playerInfo; // 전역 변수와 같이 접근한다 (ptr [rip+_playerInfo])
 ObjectInfo monsterInfo;
 
 int main()
 {
     ObjectInfo playerInfo2; // 지역 변수와 같이 접근한다 (ptr [rbp - 16])
-    // cout << playerInfo.type << " " << playerInfo2.type;
+
+    playerInfo.type = 11;
+    playerInfo2.type = 12;
+    cout << playerInfo.type << " " << playerInfo2.type;
 }
