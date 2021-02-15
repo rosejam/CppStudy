6edda17df857edff262bbdadb16704781af8ffd4
rosejam <dongju.alex@gmail.com>
2021-02-15 18:05:08 +0900

    .

--- 24.다중포인터.cpp
+++ 24.다중포인터.cpp
@@ -16,10 +16,15 @@ void SetMessage(const char* tmp) // msg
     // a[2] = 'e';
 }
 
-void SetMessage(const char** pp) // &msg
+void SetMessage(const char** pp) // pp = &msg
 {
     // pp[ &msg ] -----> msg[ Bye주소 ] ------> Bye주소[B][y][e][\0]
     *pp = "Bye"; // main의 지역변수 msg가 Bye의 시작주소를 가리키게 됨!!!
+}
+void SetMessage2(const char*& rp) // rp = msg
+{
+    // rp[ &msg ] -----> msg[ Wow주소 ] ------> Wow주소[B][y][e][\0]
+    rp = "Wow"; // main의 지역변수 msg가 Wow의 시작주소를 가리키게 됨!!!
 }
 
 int main()
@@ -37,16 +42,20 @@ int main()
 
     // (const char**) pp[ 주소1 ] -----> (const char*) 주소1[ 주소2 ] -----> (const char) 주소2[ ](1바이트 바구니) !!!! 
     //                 pp[ &msg ] ----->             msg[ Hello주소 ] ----->          Hello주소[H][e][l][l][o][\0]
-    (const char*)* pp = &msg; // 포인터의 주소를 넣는다
+    const char** pp = &msg; // 포인터의 주소를 넣는다
     
-    // [매개변수][RET][지역변수(msg = Hello주소)] [매개변수(pp = &msg)][RET][지역변수]
+    // [매개변수][RET][지역변수(msg = Hello주소)] [매개변수(&msg)][RET][지역변수]
     SetMessage(&msg);
+    cout << msg << endl;
+    
+    // [매개변수][RET][지역변수(msg = Hello주소)] [매개변수(&msg)][RET][지역변수]
+    SetMessage2(msg); // SetMessage(&msg) 와 완전히 동일!!!!
     cout << msg << endl;
 
     // 혼동스럽다?
     // 그냥 양파까기라고 생각하면 된다. 
     // 오른쪽에 있는 *부터 까면서 순간이동하면 어떤 바구니가 있는지 타고 가면 됨
-    // const char** pp2;
+    // (const char*)* pp2;
 
     return 0;
 }
