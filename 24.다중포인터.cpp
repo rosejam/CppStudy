1dab38ca0535cc56dd8a6f407b023973739c8bd2
rosejam <dongju.alex@gmail.com>
2021-02-15 17:57:30 +0900

    다중포인터

---
+++ 24.다중포인터.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+void SetNumber(int* a)
+{
+    *a = 1;
+}
+
+void SetMessage(const char* tmp) // msg
+{
+    // tmp[ Bye주소 ] ------> Bye주소[B][y][e][\0]
+    tmp = "Bye"; // 매개변수 tmp가 Hello주소를 가리키다가 Bye주소를 가리키게 됨!!!
+
+    // *a = 'B';
+    // *(a + 1) = 'y';
+    // a[2] = 'e';
+}
+
+void SetMessage(const char** pp) // &msg
+{
+    // pp[ &msg ] -----> msg[ Bye주소 ] ------> Bye주소[B][y][e][\0]
+    *pp = "Bye"; // main의 지역변수 msg가 Bye의 시작주소를 가리키게 됨!!!
+}
+
+int main()
+{
+    int a = 0;
+    SetNumber(&a);
+    // cout << a << endl; // 1
+
+    // msg[ Hello주소 ] -----> (.rodata영역) Hello주소[H][e][l][l][o][\0]
+    const char* msg = "Hello";
+    
+    // [매개변수][RET][지역변수(msg = Hello주소)] [매개변수(tmp = Hello주소)][RET][지역변수]
+    SetMessage(msg);
+    cout << msg << endl; // 그대로 Hello를 가리키는 msg
+
+    // (const char**) pp[ 주소1 ] -----> (const char*) 주소1[ 주소2 ] -----> (const char) 주소2[ ](1바이트 바구니) !!!! 
+    //                 pp[ &msg ] ----->             msg[ Hello주소 ] ----->          Hello주소[H][e][l][l][o][\0]
+    (const char*)* pp = &msg; // 포인터의 주소를 넣는다
+    
+    // [매개변수][RET][지역변수(msg = Hello주소)] [매개변수(pp = &msg)][RET][지역변수]
+    SetMessage(&msg);
+    cout << msg << endl;
+
+    // 혼동스럽다?
+    // 그냥 양파까기라고 생각하면 된다. 
+    // 오른쪽에 있는 *부터 까면서 순간이동하면 어떤 바구니가 있는지 타고 가면 됨
+    // const char** pp2;
+
+    return 0;
+}
--- 실습3.로또번호생성기.cpp
+++ 실습3.로또번호생성기.cpp
@@ -16,7 +16,54 @@ void Swap(int& a, int& b)
 
 void Sort(int numbers[], int count)
 {
+    for (int i=0; i<count; i++)
+    {
+        // i번 인덱스가 최소값을 가진다고 가정
+        int min = i;
+
+        // i번 뒤에서 진짜 최소 인덱스를 찾는다
+        for (int j=i+1; j<count; j++)
+        {
+            if(numbers[j] < numbers[min])
+                min = j;
+        }
+
+        // i번과 최소를 스왑한다
+        if (i != min)
+            Swap(numbers[i], numbers[min]);
+    }
+}
+
+void ChooseLotto(int numbers[], int count)
+{
+    // 랜덤으로 1~45 사이의 숫자 6개를 골라주세요! (단, 중복이 없어야 함)
+    srand((unsigned)time(0)); // 시드 설정
+
+    int i=0;
+    while(i < count)
+    {
+        int randValue = 1 + (rand() % 45); // 1~45
+        
+        // 이미 찾은 값이 아니라고 가정
+        bool found = false;
+        for (int j=0; j<i; j++) // (i가 0이면 실행되지 않음)
+        {
+            // 이미 찾은 값인 경우
+            if (numbers[j] == randValue)
+            {
+                found = true;
+                break;
+            }
+        }
+        // 이미 찾은 값이 아닌 경우
+        if(!found)
+        {
+            numbers[i] = randValue;
+            i++;
+        }
+    }
     
+    Sort(numbers, count);
 }
 
 int main()
@@ -25,17 +72,23 @@ int main()
     int a = 1;
     int b = 2;
     Swap(&a, &b);
-    cout << a << " " << b << endl;
+    // cout << a << " " << b << endl;
     Swap(a, b);
-    cout << a << " " << b << endl;
+    // cout << a << " " << b << endl;
 
     // 2) 정렬 함수 만들기 (작은 숫자가 먼저 오도록 정렬)
     int numbers[6] = { 1, 42, 3, 15, 5, 6 };
 
     // int size1 = sizeof(numbers); // 6*4
     // int size2 = sizeof(int); // 4
-    Sort(numbers, sizeof(numbers)/sizeof(int) ); // 배열과 배열의 길이 전달
+    Sort( numbers, sizeof(numbers)/sizeof(int) ); // 배열과 배열의 길이 전달
 
+    // 3) 로또 번호 생성기
+    ChooseLotto( numbers, sizeof(numbers)/sizeof(int) );
+
+    // 행운의 로또 앱 완성!
+    for(int i=0; i<6; i++)
+        cout << numbers[i] << endl;
 
     return 0;
 }
